bdda63253d232c89abed9c0fe1a78582b5dd221d
Joanna Chen <joanna_chen97@yahoo.com>
2024-05-13 15:51:48 -0700

    Basic functionality completed

--- src/com/password_manager/EntryReadWrite.java
+++ src/com/password_manager/EntryReadWrite.java
@@ -6,28 +6,46 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.security.*;
+import java.nio.charset.*;
+import java.util.Base64;
 
 public class EntryReadWrite {
 	static final int ENTRY_SIZE = 3;
+	// Entries in the corresponding text file should be written in the order of Entry Name, Entry Username, and Entry Password with a single space separating them.
 	static final int ENTRY_NAME_POS = 0;
-	static final int ENTRY_USERNAME_POS = 1;
-	static final int ENTRY_PASSWORD_POS = 2;
+	static final int ENTRY_USER_POS = 1;
+	static final int ENTRY_PASS_POS = 2;
+	
+	// A hash map will be used to store entries, where the key will be the entry name and the username and password will be stored as a String array.
+	static final int HASH_MAP_USER_POS = 0;
+	static final int HASH_MAP_PASS_POS = 1;
 	
 	static final String FILE_NAME = "PasswordEntries.txt";
 	
-	public static HashMap<String, String[]> entries = new HashMap<>();
-	public static File entriesFile = new File(FILE_NAME);
-	public static boolean fileChanged = false;
+	// https://www.baeldung.com/java-cipher-class
+	static final String ENCRYPT_KEY = "toEncryptDecrypt";
+	static final byte[] ENCRYPT_KEY_BYTES = ENCRYPT_KEY.getBytes();
+	static final String SEPARATOR = "-"; //\u00A9
+	private Cipher RWCipher;
+	private SecretKey cipherKey;
 	
-	public void readIn() {
-		// Entries in the corresponding text file should be written in the order of Entry Name, Entry Username, and Entry Password
-		// with a single space separating them.
+	public HashMap<String, String[]> entries = new HashMap<>();
+	public File entriesFile = new File(FILE_NAME);
+	public boolean fileChanged = false;
+	
+	public void readIn() throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, 
+    BadPaddingException, IllegalBlockSizeException {
 		boolean invalid = false;
 		
+		RWCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipherKey = new SecretKeySpec(ENCRYPT_KEY_BYTES, "AES");
+			
 		try {
 			// Try to make a new file for password entries. If it already exists, read in the entries.
 			// Default path is "C:\Users\Joanna\eclipse-workspace\PasswordManager\PasswordEntries.txt"
-			//File entriesFile = new File("PasswordEntries.txt");
 			
 			if (!entriesFile.createNewFile()) {
 				FileReader reader = new FileReader(FILE_NAME);
@@ -35,7 +53,7 @@ public class EntryReadWrite {
 				String line;
 			
 				while ((line = bufferedReader.readLine()) != null) {
-					String[] splitLine = line.split(" ");
+					String[] splitLine = decrypt(line).split("\u00A9");
 				
 					// Stop if an entry is of an invalid size
 					if (splitLine.length != ENTRY_SIZE) {
@@ -44,7 +62,7 @@ public class EntryReadWrite {
 					}
 				
 					String entryName = splitLine[ENTRY_NAME_POS];
-					String[] entryUserPass = {splitLine[ENTRY_USERNAME_POS], splitLine[ENTRY_PASSWORD_POS]};
+					String[] entryUserPass = {splitLine[ENTRY_USER_POS], splitLine[ENTRY_PASS_POS]};
 				
 					// Put into hash map using the entry name as the key
 					entries.put(entryName, entryUserPass);
@@ -61,32 +79,65 @@ public class EntryReadWrite {
 		entries = invalid ? null : entries;
 	}
 	
-	public boolean addEntry(String entryName, String entryUser, String entryPass) {
-		//TODO: Use cipher to obscure data
+	public void addEntry(String entryName, String entryUser, String entryPass) {
 		String[] entryUserPass = {entryUser, entryPass};
 		entries.put(entryName, entryUserPass);
 		
 		fileChanged = true;
-		
-		return true;
 	}
 	
-	public static void writeIn() {
+	public void deleteEntry(String entryName) {
+		entries.remove(entryName);
+		
+		fileChanged = true;
+	}
+	
+	public void writeIn() throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, 
+    BadPaddingException, IllegalBlockSizeException {
 		try {
 			entriesFile.delete();
 			entriesFile.createNewFile();
 			
 			FileWriter writer = new FileWriter(FILE_NAME);
 			
-			// Loop through each key and write it into the file
-			//for () {
-				
-			//}
+			// Loop through each key and write it into the file. The name of the entry, username, and password are separated by a copyright symbol.
+			for (String name : entries.keySet()) {
+				writer.append(encrypt(name + "\u00A9" + entries.get(name)[HASH_MAP_USER_POS] + "\u00A9" + entries.get(name)[HASH_MAP_PASS_POS]) + "\n");
+			}
 			writer.close();
 		}
 		
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	// https://www.baeldung.com/java-cipher-class
+	private String encrypt(String toEncrypt) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, 
+    BadPaddingException, IllegalBlockSizeException {
+		RWCipher.init(Cipher.ENCRYPT_MODE, cipherKey);
+		
+		byte[] encrypted = RWCipher.doFinal(toEncrypt.getBytes(StandardCharsets.UTF_8));
+		byte[] encoded = Base64.getEncoder().encode(encrypted);
+		
+		return new String(encoded);
+	}
+	
+	private String decrypt(String toDecode) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, 
+    BadPaddingException, IllegalBlockSizeException {
+		RWCipher.init(Cipher.DECRYPT_MODE, cipherKey);
+		
+		byte[] decoded = Base64.getDecoder().decode(toDecode.getBytes());
+		byte[] decrypted = RWCipher.doFinal(decoded);
+		
+		return new String(decrypted, StandardCharsets.UTF_8);
+	}
+	
+	public HashMap<String, String[]> getEntries() {
+		return entries;
+	}
+	
+	public boolean isFileChanged() {
+		return fileChanged;
 	}
 }
--- src/com/password_manager/EntryWindow.java
+++ src/com/password_manager/EntryWindow.java
@@ -1,83 +1,337 @@
 package com.password_manager;
 
-import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
-//import java.io.Serializable;
-import com.password_manager.EntryReadWrite;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
-public class EntryWindow extends Frame {
-	private static final long serialVersionUID = 7043752297123044721L;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class EntryWindow {
+	private static final int WINDOW_INIT_WIDTH = 750;
+	private static final int WINDOW_INIT_HEIGHT = 500;
+	private static final String PASSWORD = "10pacesleft";
+
+	private static final String[] tableColumnNames = {"Entry Name", "Username", "Password"};
 	
-	static final int WINDOW_INIT_WIDTH = 750;
-	static final int WINDOW_INIT_HEIGHT = 500;
+	private JTabbedPane tabPanel;
 	
-	public EntryWindow() {
-		TextField nameField = new TextField();
-		TextField userField = new TextField();
-		TextField passField = new TextField();
-		Button addButton = new Button();
-		
-		// read entries and if it returns an empty hash map, put an error message and exit
-		EntryReadWrite entryRW = new EntryReadWrite();
-		entryRW.readIn();
-		
-		if (EntryReadWrite.entries == null) {
-			//TODO: Error reading the entry text file
+	private JPanel entrySavePanel;
+	private JTextField nameField;
+	private JTextField userField;
+	private JTextField passField;
+	private JLabel nameFieldLabel;
+	private JLabel userFieldLabel;
+	private JLabel passFieldLabel;
+	private JButton saveButton;
+	
+	private JPanel entrySearchPanel;
+	private JLabel searchFieldLabel;
+	private JTextField searchField;
+	private JTable searchTable;
+	private DefaultTableModel searchTableDTM;
+	private JScrollPane searchTableScroll;
+	private JButton searchButton;
+	private JButton deleteButton;
+	
+	private EntryReadWrite entryRW;
+	
+	public EntryWindow() throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, 
+    BadPaddingException, IllegalBlockSizeException {
+		// Enter password to access entries
+		while (true) {
+			String passCheck = JOptionPane.showInputDialog("Enter password:");
+			if (passCheck == null) {
+				System.exit(0);
+			}
+			else if (passCheck.equals(PASSWORD)) {
+				break;
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Incorrect password.", "Notice", JOptionPane.WARNING_MESSAGE);
+			}
 		}
 		
-		Frame window = new Frame("Password Manager");
+		// Read entries and if it returns an empty hash map, put an error message and exit
+		entryRW = new EntryReadWrite();
+		entryRW.readIn();
+		
+		if (entryRW.getEntries() == null) {
+			JOptionPane.showMessageDialog(null, "Unable to load entries.", "Notice", JOptionPane.WARNING_MESSAGE);
+			System.exit(0);
+		}
+		
+		JFrame window = new JFrame("Password Manager");
 		frameSetUp(window);
-		
-		addButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (EntryReadWrite.entries.containsKey(nameField.getText())) {
-					// TODO: Error message - no dupes
-				}
-				else if (nameField.getText().isBlank() || nameField.getText().contains(" ") ||
-						userField.getText().isBlank() || userField.getText().contains(" ") ||
-						passField.getText().isBlank() || passField.getText().contains(" ")) {
-					//TODO: Error message - invalid input
-				}
-				else {
-					entryRW.addEntry(nameField.getText(), userField.getText(), passField.getText());
-				}
-			}
-		});
-		
-		window.add(nameField);
-		window.add(userField);
-		window.add(passField);
-		window.add(addButton);
 	}
 	
 	//https://docs.oracle.com/javase%2F7%2Fdocs%2Fapi%2F%2F/java/awt/Frame.html
 	//https://docs.oracle.com/javase/tutorial/uiswing/events/windowlistener.html
 	//https://docs.oracle.com/javase%2F7%2Fdocs%2Fapi%2F%2F/java/awt/event/WindowEvent.html
-	//https://www.wikihow.com/Close-a-Window-in-Java
-	public void frameSetUp(Frame f) {
+	//https://www.codejava.net/java-se/swing/jframe-basic-tutorial-and-examples
+	//https://www.javatpoint.com/java-gridbaglayout
+	//https://www.geeksforgeeks.org/java-jtabbedpane/
+	//https://www.geeksforgeeks.org/java-swing-jtable/
+	
+	public void frameSetUp(JFrame f) {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int frameInitX = (int)screenSize.getWidth()/2;
 		int frameInitY = (int)screenSize.getHeight()/2;
 		f.setBounds(frameInitX - WINDOW_INIT_WIDTH/2, frameInitY - WINDOW_INIT_HEIGHT/2, WINDOW_INIT_WIDTH, WINDOW_INIT_HEIGHT);
-		//f.addWindowListener();
+		
+		tabPanel = new JTabbedPane();
+		
+		savePanelSetUp();
+		searchPanelSetUp();
+		
+		tabPanel.addTab("Add Entries", entrySavePanel);
+		tabPanel.addTab("Search Entries", entrySearchPanel);
+		
+		// Save any changes if entries were added/deleted.
 		f.addWindowListener(new WindowAdapter() {
-			@Override
 			public void windowClosing(WindowEvent e) {
-				if (EntryReadWrite.fileChanged) {
-					EntryReadWrite.writeIn();
+				if (entryRW.isFileChanged()) {
+					try {
+						entryRW.writeIn();
+					}
+					catch (InvalidKeyException exception) {
+						exception.printStackTrace();
+					}
+					catch (NoSuchPaddingException exception) {
+						exception.printStackTrace();
+					}
+					catch (NoSuchAlgorithmException exception) {
+						exception.printStackTrace();
+					}
+					catch (BadPaddingException exception) {
+						exception.printStackTrace();
+					}
+					catch (IllegalBlockSizeException exception) {
+						exception.printStackTrace();
+					}
 				}
 				
 				System.exit(0);
 			}
 		});
 		
+		f.add(tabPanel);
 		f.setVisible(true);
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void savePanelSetUp() {
+		entrySavePanel = new JPanel();
+		entrySavePanel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(5, 5, 5, 5);
+		
+		nameFieldLabel = new JLabel("Entry Name");
+		c.gridx = 0;
+		c.gridy = 0;
+		entrySavePanel.add(nameFieldLabel, c);
+		
+		nameField = new JTextField();
+		nameField.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					saveEntry();
+				}
+			}
+		});;
+		c.ipadx = 100;
+		c.gridx = 1;
+		c.gridy = 0;
+		entrySavePanel.add(nameField, c);
+		
+		userFieldLabel = new JLabel("Entry Username");
+		c.ipadx = 0;
+		c.gridx = 0;
+		c.gridy = 1;
+		entrySavePanel.add(userFieldLabel, c);
+		
+		userField = new JTextField();
+		userField.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					saveEntry();
+				}
+			}
+		});
+		c.ipadx = 100;
+		c.gridx = 1;
+		c.gridy = 1;
+		entrySavePanel.add(userField, c);
+		
+		passFieldLabel = new JLabel("Entry Password");
+		c.ipadx = 0;
+		c.gridx = 0;
+		c.gridy = 2;
+		entrySavePanel.add(passFieldLabel, c);
+		
+		passField = new JTextField();
+		passField.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					saveEntry();
+				}
+			}
+		});
+		c.ipadx = 100;
+		c.gridx = 1;
+		c.gridy = 2;
+		entrySavePanel.add(passField, c);
+		
+		saveButton = new JButton("Save");
+		c.ipadx = 0;
+		c.gridx = 0;
+		c.gridy = 3;
+		c.gridwidth = 2;
+		
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				saveEntry();
+			}
+		});
+		
+		entrySavePanel.add(saveButton, c);
+	}
+	
+	public void searchPanelSetUp() {
+		entrySearchPanel = new JPanel();
+		entrySearchPanel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(5, 5, 5, 5);
+		
+		searchFieldLabel = new JLabel("Search Entry Name");
+		c.gridx = 0;
+		c.gridy = 0;
+		entrySearchPanel.add(searchFieldLabel, c);
+		
+		searchField = new JTextField();
+		searchField.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER && !searchField.getText().isBlank()) {
+					searchEntry();
+				}
+			}
+		});
+		c.ipadx = 100;
+		c.gridx = 0;
+		c.gridy = 1;
+		entrySearchPanel.add(searchField, c);
+		
+		searchTableDTM = new DefaultTableModel();
+		searchTable = new JTable(searchTableDTM);
+		
+		for (String name : tableColumnNames) {
+			searchTableDTM.addColumn(name);
+		}
+		
+		searchTableScroll = new JScrollPane(searchTable);
+		c.ipadx = 500;
+		c.ipady = 100;
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridwidth = 3;
+		c.gridheight = 4;
+		entrySearchPanel.add(searchTableScroll, c);
+		
+		searchButton = new JButton("Search");
+		c.ipadx = 0;
+		c.ipady = 0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.gridx = 0;
+		c.gridy = 2;
+		
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!searchField.getText().isBlank()) {
+					searchEntry();
+				}
+			}
+		});
+		
+		entrySearchPanel.add(searchButton, c);
+		
+		deleteButton = new JButton("Delete");
+		c.gridx = 0;
+		c.gridy = 3;
+		
+		deleteButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (searchTable.getSelectedRow() >= 0) {
+					int decision = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete the selected entry?", "Notice", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+					
+					if (decision == JOptionPane.YES_OPTION) {
+						entryRW.deleteEntry((String)searchTable.getValueAt(0, searchTable.getSelectedRow()));
+						searchTableDTM.removeRow(searchTable.getSelectedRow());
+						
+						searchTableDTM.fireTableDataChanged();
+						searchTable.revalidate();
+					}
+				}
+			}
+		});
+		
+		entrySearchPanel.add(deleteButton, c);
+		
+		// Fill up the search table with all entries
+		String[] entryArray = new String[EntryReadWrite.ENTRY_SIZE];
+		
+		for (String entry : entryRW.entries.keySet()) {
+			entryArray[EntryReadWrite.ENTRY_NAME_POS] = entry;
+			entryArray[EntryReadWrite.ENTRY_USER_POS] = entryRW.getEntries().get(entry)[EntryReadWrite.HASH_MAP_USER_POS];
+			entryArray[EntryReadWrite.ENTRY_PASS_POS] = entryRW.getEntries().get(entry)[EntryReadWrite.HASH_MAP_PASS_POS];
+			searchTableDTM.addRow(entryArray);
+		}
+		
+		searchTableDTM.fireTableDataChanged();
+		searchTable.revalidate();
+	}
+	
+	private void saveEntry() {
+		if (entryRW.entries.containsKey(nameField.getText())) {
+			JOptionPane.showMessageDialog(null, "Duplicate entry found. Unable to save entries with the same name.", "Notice", JOptionPane.WARNING_MESSAGE);
+		}
+		else if (nameField.getText().isBlank() ||
+				userField.getText().isBlank() || userField.getText().contains(" ") ||
+				passField.getText().isBlank() || passField.getText().contains(" ")) {
+			JOptionPane.showMessageDialog(null, "Invalid input. The name of the entry cannot be blank. The username or password cannot be blank or contain spaces.", "Notice", JOptionPane.WARNING_MESSAGE);
+		}
+		else {
+			entryRW.addEntry(nameField.getText(), userField.getText(), passField.getText());
+			JOptionPane.showMessageDialog(null, "Entry saved.", "Notice", JOptionPane.INFORMATION_MESSAGE);
+			nameField.setText("");
+			userField.setText("");
+			passField.setText("");
+		}
+	}
+	
+	private void searchEntry() {
+		String[] matchingEntry = new String[EntryReadWrite.ENTRY_SIZE];
+		
+		searchTableDTM.setNumRows(0);
+		
+		for (String entry : entryRW.entries.keySet()) {
+			if (entry.toLowerCase().contains(searchField.getText())) {
+				matchingEntry[EntryReadWrite.ENTRY_NAME_POS] = entry;
+				matchingEntry[EntryReadWrite.ENTRY_USER_POS] = entryRW.getEntries().get(entry)[EntryReadWrite.HASH_MAP_USER_POS];
+				matchingEntry[EntryReadWrite.ENTRY_PASS_POS] = entryRW.getEntries().get(entry)[EntryReadWrite.HASH_MAP_PASS_POS];
+				searchTableDTM.addRow(matchingEntry);
+			}
+		}
+		
+		searchTableDTM.fireTableDataChanged();
+		searchTable.revalidate();
+	}
+	
+	public static void main(String[] args) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, 
+    BadPaddingException, IllegalBlockSizeException {
 		new EntryWindow();
 	}
 }
